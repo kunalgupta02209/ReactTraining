3f9066a94905783fe08b9f1d0693e7adafb25804
Kunal Gupta <kunalgupta02209@gmail.com>
2022-07-10 12:08:10 +0530

    Add season Display FIle

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeasonDisplay = (props) => {
+  return <div>Season {props.lat}</div>;
+};
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  render() {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    console.log("Component did mount.");
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.error(error)
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
     );
-    return <div>Latitude:</div>;
+  }
+
+  componentDidCatch() {
+    super.componentDidCatch();
+    console.log("COmponent Did catch called");
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>;
+    }
+
+    return <div>Loading</div>;
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
